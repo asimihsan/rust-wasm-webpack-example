2f7848d08d26f90879ddcea58eb53a74c9619eac
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-02-21 10:07:36 +0000

    relicense as AGPL-3

--- web/apply_critical.py
+++ web/apply_critical.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+#  Copyright (C) 2023 Asim Ihsan
+#  SPDX-License-Identifier: AGPL-3.0-only
+#
+#  This program is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Affero General Public License as published by the Free
+#  Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#  PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License along
+#  with this program. If not, see <https://www.gnu.org/licenses/>
+
 from pathlib import Path
 from string import Template
 from typing import List
